6b691d3df27be9d00b2d806bef2bff3da4ffd2db
seonbii08 <121408547+seonbii08@users.noreply.github.com>
2023-05-09 10:33:59 +0900

    inputbox update

--- react-study/src/pages/todos/CreateItemBox/styled.js
+++ react-study/src/pages/todos/CreateItemBox/styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const InputBox = styled.div`
   display: flex;
+  justify-content: center;
   width: 100%;
 `;
 
--- react-study/src/pages/todos/styled.js
+++ react-study/src/pages/todos/styled.js
@@ -4,20 +4,9 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   width: 60vw;
   height: 100vh;
   margin: 0 auto;
 `;
 
 export const Title = styled.h2``;
-
-export const InputBox = styled.div`
-  display: flex;
-  width: 100%;
-`;
-
-export const Input = styled.input`
-  width: 100%;
-  padding: 4px;
-`;
