4b11517605692c6be3cdfb4e23c8bcd6b1dc23c0
seonbii08 <121408547+seonbii08@users.noreply.github.com>
2023-05-09 11:20:32 +0900

    todo update

--- react-study/src/pages/todos/index.jsx
+++ react-study/src/pages/todos/index.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+
 import * as S from './styled';
 
 import CreateItemBox from './CreateItemBox';
+import ItemList from './ItemList';
 
 function Todos() {
+  const [todoName, setTodoName] = useState('');
+  const [items, setItems] = useState([]);
+
   return (
     <S.Container>
       <S.Title>To do list</S.Title>
-      <CreateItemBox />
+      <CreateItemBox onChange={setTodoName} />
+      <ItemList />
     </S.Container>
   );
 }
