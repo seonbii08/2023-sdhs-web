74f6740f16562363f2977c66f9514b50086aa4b3
seonbii08 <121408547+seonbii08@users.noreply.github.com>
2023-05-10 11:17:42 +0900

    Todo

--- react-study/src/components/Todo/styled.js
+++ react-study/src/components/Todo/styled.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components';
+import { Button } from '../Button/styled';
+export const Container = styled.li`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid black;
+  > ${Button} {
+    position: relative;
+    right: 0px;
+    bottom: -1px;
+    width: 50px;
+    outline: 0;
+    border: 0;
+    background-color: black;
+    color: #fff;
+    height: 30px;
+  }
+  > ${Button}:nth-child(2n + 1) {
+    background-color: red;
+    border-radius: 0 5px 0 0;
+  }
 
-export const Container = styled.li``;
+  > ${Button}:nth-child(2n) {
+    border-radius: 5px 0 0 0;
+  }
+`;
 
 export const Title = styled.p`
   font-size: 16px;
+  width: 175px;
 `;
--- react-study/src/pages/todos/index.jsx
+++ react-study/src/pages/todos/index.jsx
@@ -4,19 +4,25 @@ import * as S from './styled';
 
 import CreateItemBox from './CreateItemBox';
 import ItemList from './ItemList';
+import Button from '../../components/Button';
 
 function Todos() {
   const [todoName, setTodoName] = useState('');
   const [todos, setTodos] = useState([]);
 
   const createTodo = () => {
+    if (!todoName.trim()) {
+      alert('값이 없습니다.');
+      return;
+    }
+    setTodoName('');
     setTodos(prevState => [...prevState, { id: prevState.length + 1, name: todoName }]);
   };
 
   return (
     <S.Container>
       <S.Title>To do list</S.Title>
-      <CreateItemBox onChange={setTodoName} createTodo={createTodo} />
+      <CreateItemBox value={todoName} onChange={setTodoName} createTodo={createTodo} />
       <ItemList todos={todos} />
     </S.Container>
   );
