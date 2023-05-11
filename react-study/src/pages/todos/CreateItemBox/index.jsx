92c440afb6a5b1054b9d23d9d3080299fa1fbba7
seonbii08 <121408547+seonbii08@users.noreply.github.com>
2023-05-11 10:38:52 +0900

    CreateItemBox update

--- react-study/src/pages/todos/CreateItemBox/index.jsx
+++ react-study/src/pages/todos/CreateItemBox/index.jsx
@@ -1,18 +1,30 @@
-import Button from '../../../components/Button';
+import React from 'react';
 import * as S from './styled';
+import Button from '../../../components/Button';
 
-function CreateItemBox({ value, onChange, createTodo }) {
-  const handleInputValue = e => {
+export default function CreateItemBox({ value, onChange, createTodo }) {
+  const handleInput = e => {
     onChange(e.target.value);
   };
+
+  const handleEnterPress = e => {
+    const input = e.target.value;
+    if (!input.trim()) return;
+
+    if (e.key === 'Enter') createTodo();
+  };
+
   return (
     <S.InputBox>
-      <S.Input value={value} placeholder='To do를 입력해주세요.' onChange={handleInputValue} />
+      <S.Input
+        onKeyUp={handleEnterPress}
+        value={value}
+        onChange={handleInput}
+        placeholder='Todo를 입력해주세요'
+      />
       <Button onClick={createTodo} disabled={!value.trim()}>
         등록
       </Button>
     </S.InputBox>
   );
 }
-
-export default CreateItemBox;
