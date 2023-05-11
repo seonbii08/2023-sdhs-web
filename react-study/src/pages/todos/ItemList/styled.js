c572c39a81727daf8181112fa26b0e7a5933ae0a
seonbii08 <121408547+seonbii08@users.noreply.github.com>
2023-05-11 10:39:58 +0900

    itemlist style update

--- react-study/src/pages/todos/ItemList/styled.js
+++ react-study/src/pages/todos/ItemList/styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
-export const ItemList = styled.ul`
+export const ItemList = styled.div`
   display: flex;
+  width: 100%;
   flex-direction: column;
   gap: 8px;
 `;
