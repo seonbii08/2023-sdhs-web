6e85267dd4bbbc686e56b808eec19b98df4ae144
seonbii08 <121408547+seonbii08@users.noreply.github.com>
2023-05-25 15:13:10 +0900

    main update

--- todo-with-ts/src/App.tsx
+++ todo-with-ts/src/App.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, useState} from 'react';
+import {ChangeEvent, useState, KeyboardEvent, useEffect} from 'react';
 
 import Input from "./components/Input";
 
@@ -8,19 +8,30 @@ import type { Todo } from "./types/todo";
 
 import {v4 as uuidv4} from 'uuid';
 
+import * as S from './styled';
+
 
 
 function App() {
     const [todoName, setTodoName] = useState<Todo['name']>("");
     const [todos, setTodos] = useState<Todo[]>([]);
     const [searchValue,  setSearchValue] = useState<string>("")
-    const [selectedTodoIds, setSelectedTodoIds] = useState<Todo["id"][]>([])
+    const [selectedTodoIds, setSelectedTodoIds] = useState<Todo["id"][]>([]);
+    const [editedTodoId, setEditedTodoId] = useState<Todo['id'] | undefined>();
+    const [editedName, setEditedName] = useState<Todo["name"]>('');
 
     const handleTodoName = ({
         target: {value},
      }: ChangeEvent<HTMLInputElement>) => {
         setTodoName(value);
     };
+    
+    const handleEnterPress = (e:KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === "Enter"){
+            // setTodos((prevState) => [...prevState, {id: uuidv4(), name:todoName}]);
+            addTodo();
+        }
+    }
 
     const handleSearchValue = ({
         target: {value},
@@ -37,18 +48,55 @@ function App() {
 
     const addTodo = () => {
         setTodos((prevState) => [...prevState, {id: uuidv4(), name: todoName }])
+        // setTodoName("");
     };
+
+    const toggleEditTodo = (id: Todo['id']) => {
+        setEditedTodoId((prevState) => (prevState === id ? undefined : id));
+            // if(prevState === id) {
+            //     return undefined;
+            // }
+            // return id;
+        }
+
+    const handleEditedName = (name: Todo['name']) => {
+        setEditedName(name);
+    }
+
+    const editTodo = () => {
+        setTodos((prevState) => {
+            return prevState.map(({id, name}) => {
+                if(id === editedTodoId) {
+                    return {id, name:editedName}
+                }
+                return {id, name};
+            });
+        });
+        setEditedTodoId(undefined);
+        setEditedName("");
+    };
+    
+
+    useEffect(() => {
+        setTodoName("");
+    }, [todos])
+
+
     return (
         <div className="App">
-            <Input onChange={handleTodoName} />
+            <Input value={todoName} onChange={handleTodoName} onKeyUp={handleEnterPress} placeholder="메시지를 입력하세요" />
             <button onClick={addTodo}>Todo 추가</button>
-            <Input onChange={handleSearchValue}/>
-            {todoName}
+            <Input onChange={handleSearchValue} placeholder="검색"/>
             <TodoList 
                 todos={todos} 
                 searchValue={searchValue} 
+                editedTodoId={editedTodoId}
                 deleteTodo={handleDeleteTodo}
                 setSelectedTodoIds={setSelectedTodoIds}
+                toggleEditTodo={toggleEditTodo}
+                setEditedName={handleEditedName}
+                editTodo={editTodo}
+
             />
         </div>
     );
