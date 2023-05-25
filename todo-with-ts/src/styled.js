abe4ce50e5ce383bfd9c1525569018feb6d0d2fe
seonbii08 <121408547+seonbii08@users.noreply.github.com>
2023-05-25 15:13:25 +0900

    style

---
+++ todo-with-ts/src/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  width: 60vw;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 16px;
+`;
+
+export const Title = styled.h2``;
